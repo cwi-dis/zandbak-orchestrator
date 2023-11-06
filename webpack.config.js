4b8ab9cb37d0bb487d98c3bd643bbbeeb2b1714d
Thomas Röggla <t.roggla@cwi.nl>
2023-11-06 15:20:33 +0100

    added typescript config

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require("webpack");
+
+const backend = {
+  mode: process.env.NODE_ENV || "development",
+  target: "node",
+  node: {
+    __dirname: true
+  },
+  entry: {
+    build: "./app.ts",
+  },
+  output: {
+    path: __dirname + "/bin",
+    filename: "[name].js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".ts"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader"
+      }
+    ]
+  }
+};
+
+module.exports = backend;
