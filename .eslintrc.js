310df74bcf56cfa2d5f41259559f7f84b7f133e4
Thomas Röggla <t.roggla@cwi.nl>
2023-11-06 15:24:38 +0100

    added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "ignorePatterns": [
+    "node_modules/",
+    "bin/",
+    "webpack.config.js"
+  ],
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "@typescript-eslint"
+  ],
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ],
+    "no-trailing-spaces": "error"
+  }
+};
