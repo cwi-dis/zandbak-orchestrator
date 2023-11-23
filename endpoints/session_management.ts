1a1bcdc4ccb2c8f4020aa2bdb276f19e903bedc9
Thomas Röggla <tom@lichtfragmente.com>
2023-11-23 13:34:15 +0100

    extracting scenario data when creating new session

--- endpoints/session_management.ts
+++ endpoints/session_management.ts
@@ -5,17 +5,23 @@ import Orchestrator from "../app/orchestrator";
 import User from "../app/user";
 import ErrorCodes  from "./error_codes";
 import Session from "../app/session";
+import Scenario from "app/scenario";
 
 const installHandlers = (orchestrator: Orchestrator, user: User) => {
   const { socket } = user;
 
   socket.on(EndpointNames.ADD_SESSION, (data, callback) => {
     const {
-      sessionName, sessionDescription,
-      sessionProtocol = "unknown"
+      sessionName, sessionDescription, sessionProtocol = "unknown",
+      scenarioDefinition: { scenarioId, scenarioName, scenarioDescription }
     } = data;
 
-    const session = new Session(sessionName.trim(), sessionDescription, sessionProtocol);
+    const session = new Session(
+      sessionName.trim(),
+      sessionDescription,
+      sessionProtocol,
+      new Scenario(scenarioId, scenarioName, scenarioDescription)
+    );
 
     session.addUser(user);
     session.setAdministrator(user);
