b8ed9679a58f9d0c5c94e0400160f387ae832e5d
Thomas Röggla <t.roggla@cwi.nl>
2023-12-05 17:17:56 +0100

    added handler for removing data streams

--- endpoints/data_streams.ts
+++ endpoints/data_streams.ts
@@ -40,6 +40,36 @@ const installHandlers = (user: User) => {
       connectionLoggedAs: user.id
     }));
   });
+
+  /**
+   * Removes a data stream for the current user by taking stream type as
+   * parameter. Returns a serialised list of connection information and
+   * declared data streams after the removal operation
+   */
+  socket.on(EndpointNames.REMOVE_DATA_STREAM, (streamType, callback) => {
+    const { session } = user;
+
+    if (!session) {
+      return callback(util.createResponse(
+        ErrorCodes.SESSION_USER_NOT_IN_ANY_SESSION
+      ));
+    }
+
+    if (!streamType) {
+      return callback(util.createResponse(
+        ErrorCodes.STREAM_DATA_MISSING_KIND
+      ));
+    }
+
+    user.removeDataStream(streamType);
+    const { dataStreams, remoteDataStreams } = user.serialize();
+
+    callback(util.createResponse(ErrorCodes.OK, {
+      dataStreams, remoteDataStreams,
+      connectionId: user.id,
+      connectionLoggedAs: user.id
+    }));
+  });
 };
 
 export default installHandlers;
