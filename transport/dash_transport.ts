924d82460fe2f3faaf896de473066635480a9b93
Thomas Röggla <t.roggla@cwi.nl>
2023-12-07 18:03:22 +0100

    added type definitions for shape of SFU configs

--- transport/dash_transport.ts
+++ transport/dash_transport.ts
@@ -1,5 +1,24 @@
 import ExternalTransport from "./external_transport";
 
+interface DashTransportPortMapping {
+  port: number;
+  sfuData: {
+    url_gen: string,
+    url_audio: string,
+    url_pcc: string
+  }
+}
+
+interface DashTransportConfig {
+  tls: boolean;
+  autorestart: boolean;
+  log: boolean;
+  logPrefix: string;
+  logSuffix: string;
+  commandLine: Array<string>
+  portMapping: Array<DashTransportPortMapping>
+}
+
 class DashTransport extends ExternalTransport {
   protected cmdLine: Array<string>;
   protected port: number;
--- transport/webrtc_transport.ts
+++ transport/webrtc_transport.ts
@@ -1,5 +1,24 @@
 import ExternalTransport from "./external_transport";
 
+interface WebRTCTransportPortMapping {
+  port: number;
+  sfuData: {
+    url_gen: string,
+    url_audio: string,
+    url_pcc: string
+  }
+}
+
+interface WebRTCTransportConfig {
+  tls: boolean;
+  autorestart: boolean;
+  log: boolean;
+  logPrefix: string;
+  logSuffix: string;
+  commandLine: Array<string>
+  portMapping: Array<WebRTCTransportPortMapping>
+}
+
 class WebRTCTransport extends ExternalTransport {
   protected cmdLine: Array<string>;
   protected port: number;
