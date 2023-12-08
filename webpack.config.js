38c7a911451dd15b15257280eda53f8fcc0826bc
Thomas Röggla <t.roggla@cwi.nl>
2023-12-08 19:15:14 +0100

    added webpack config for client module

--- webpack.config.js
+++ webpack.config.js
@@ -29,4 +29,32 @@ const backend = {
   }
 };
 
-module.exports = backend;
+const client = {
+  mode: process.env.NODE_ENV || "development",
+  target: "node",
+  node: {
+    __dirname: true
+  },
+  externals: [nodeExternals()],
+  entry: {
+    client: "./client.ts",
+  },
+  output: {
+    path: __dirname + "/bin",
+    filename: "[name].js"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".ts"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader"
+      }
+    ]
+  }
+}
+
+module.exports = [backend, client];
