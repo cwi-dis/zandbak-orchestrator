e263019708bb9d3d539bb003be94330766618519
Thomas Röggla <tom@lichtfragmente.com>
2023-12-13 14:36:22 +0100

    added test calls to client script

--- client.ts
+++ client.ts
@@ -2,10 +2,22 @@ import { io } from "socket.io-client";
 
 const socket = io("ws://localhost:8090");
 
-socket.on("connect", () => {
+socket.on("connect", async () => {
   console.log("connected!");
 
-  socket.emit("Login", { userName: "test" }, (res: any) => {
+  await socket.emit("Login", { userName: "test" }, (res: any) => {
     console.log(res);
+  });
+
+  await socket.emit("GetUserData", { userId: "666" }, (res: any) => {
+    console.log("User data:", res);
+  });
+
+  await socket.emit("GetOrchestratorVersion", {}, (res: any) => {
+    console.log("Orchestrator version:", res);
+  });
+
+  await socket.emit("GetNTPTime", {}, (res: any) => {
+    console.log("Orchestrator version:", res);
   });
 });
