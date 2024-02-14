8569a595a1cf59c907694f6dd262f6a57d050a5b
Thomas Röggla <t.roggla@cwi.nl>
2024-02-14 17:33:04 +0100

    instantiating DummyTransport if sessionProtocol is 'unknown'

--- transport/transport_manager.ts
+++ transport/transport_manager.ts
@@ -1,5 +1,6 @@
 import DashTransport from "./dash_transport";
 import WebRTCTransport from "./webrtc_transport";
+import DummyTransport from "./dummy_transport";
 
 export type TransportType = "dash" | "webrtc" | "socketio" | "unknown";
 
@@ -9,9 +10,10 @@ class TransportManager {
     case "webrtc":
       return new WebRTCTransport();
     case "dash":
+      return new DashTransport();
     case "unknown":
     default:
-      return new DashTransport();
+      return new DummyTransport();
     }
   }
 }
