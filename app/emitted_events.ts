0459881158879f158a71a58f6d8bb0ac6dfa7030
Thomas Röggla <t.roggla@cwi.nl>
2024-02-17 20:12:43 +0100

    added enum for emitted events

---
+++ app/emitted_events.ts
@@ -0,0 +1,12 @@
+enum EmittedEvents {
+  SESSION_CLOSED = "SessionClosed",
+  SESSION_UPDATED = "SessionUpdated",
+
+  MESSAGE_SENT = "MessageSent",
+  DATA_RECEIVED = "DataReceived",
+
+  SCENE_EVENT_TO_MASTER = "SceneEventToMaster",
+  SCENE_EVENT_TO_USER = "SceneEventToUser"
+}
+
+export default EmittedEvents;
