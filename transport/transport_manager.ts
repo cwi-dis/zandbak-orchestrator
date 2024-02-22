53ce82551108e463abade3d2278a5796a068a81f
Thomas Röggla <tom@lichtfragmente.com>
2024-02-22 01:36:02 +0100

    TransportManager reads EXTERNAL_HOSTNAME from environment

--- transport/transport_manager.ts
+++ transport/transport_manager.ts
@@ -1,16 +1,21 @@
+import { getFromEnvironment } from "../util";
+
+import Transport from "./transport";
 import DashTransport from "./dash_transport";
 import WebRTCTransport from "./webrtc_transport";
 import DummyTransport from "./dummy_transport";
 
 export type TransportType = "dash" | "webrtc" | "socketio" | "unknown";
 
+const [ EXTERNAL_HOSTNAME ] = getFromEnvironment(["EXTERNAL_HOSTNAME"], null);
+
 class TransportManager {
-  public static instantiate(protocol: TransportType) {
+  public static instantiate(protocol: TransportType): Transport {
     switch (protocol) {
     case "webrtc":
-      return new WebRTCTransport();
+      return new WebRTCTransport(EXTERNAL_HOSTNAME);
     case "dash":
-      return new DashTransport();
+      return new DashTransport(EXTERNAL_HOSTNAME);
     case "unknown":
     default:
       return new DummyTransport();
