b982e24ed443504adf4983c896ceb82cbe456ccc
Thomas Röggla <tom@lichtfragmente.com>
2024-02-22 01:38:08 +0100

    implemented getUrls for DashTransport and WebRTCTransport

--- transport/dash_transport.ts
+++ transport/dash_transport.ts
@@ -25,6 +25,26 @@ class DashTransport extends ExternalTransport {
   protected cmdLine: Array<string> = this.transportConfig.commandLine;
   protected tls: boolean = this.transportConfig.tls;
   protected port: number;
+
+  private buildUrl(urlTemplate: string, user: User) {
+    return urlTemplate.replace(
+      "%EXTERNAL_HOSTNAME%", this.externalHostname
+    ).replace(
+      "%SESSION_ID%", user.session?.id || ""
+    ).replace(
+      "%USER_ID%", user.id
+    );
+  }
+
+  public getUrls(user: User) {
+    const sfuUrls = this.transportConfig.portMapping[this.port].sfuData;
+
+    return {
+      url_gen: this.buildUrl(sfuUrls.url_gen, user),
+      url_audio: this.buildUrl(sfuUrls.url_audio, user),
+      url_pcc: this.buildUrl(sfuUrls.url_pcc, user)
+    };
+  }
 }
 
 export default DashTransport;
--- transport/webrtc_transport.ts
+++ transport/webrtc_transport.ts
@@ -24,6 +24,22 @@ class WebRTCTransport extends ExternalTransport {
   protected cmdLine: Array<string> = this.transportConfig.commandLine;
   protected tls: boolean = this.transportConfig.tls;
   protected port: number;
+
+  private buildUrl(urlTemplate: string) {
+    return urlTemplate.replace(
+      "%EXTERNAL_HOSTNAME%", this.externalHostname
+    );
+  }
+
+  public getUrls() {
+    const sfuUrls = this.transportConfig.portMapping[this.port].sfuData;
+
+    return {
+      url_gen: this.buildUrl(sfuUrls.url_gen),
+      url_audio: this.buildUrl(sfuUrls.url_audio),
+      url_pcc: this.buildUrl(sfuUrls.url_pcc),
+    };
+  }
 }
 
 export default WebRTCTransport;
