1ec56e826e72ee242f211df71e878ce5e98c7e72
Thomas Röggla <tom@lichtfragmente.com>
2024-02-25 05:04:02 +0100

    cleaned up logreader script

--- logreader/index.js
+++ logreader/index.js
@@ -1,7 +1,9 @@
 const io = require("socket.io-client");
 
 if (process.argv.length < 3) {
-  console.log("USAGE:", process.argv[0], process.argv[1], "orchestrator_host");
+  const [ command, script ] = process.argv;
+  console.log("USAGE:", command, script, "orchestrator_host");
+
   process.exit(1);
 }
 
@@ -9,11 +11,11 @@ const [ , , host ] = process.argv;
 const url = new URL("/log", "ws://" + host);
 const socket = io(url.href);
 
-socket.on("connect", async () => {
+socket.on("connect", () => {
   console.log("Connected to", url.href);
   console.log("Ready to receive log messages...");
 
-  socket.on("message", ({ level, message, timestamp }) => {
-    console.log(`${timestamp} ${level}:`, message);
+  socket.on("message", ({ timestamp, level, message }) => {
+    console.log(timestamp, `${level}:`, message);
   });
 });
