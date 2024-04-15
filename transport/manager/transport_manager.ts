8002106b0173135bf42ebcf38d39630461f85991
Thomas Röggla <tom@lichtfragmente.com>
2024-04-15 15:19:34 +0200

    TransportManager#assignExternalTransport returns DummyTransport on error

--- transport/manager/transport_manager.ts
+++ transport/manager/transport_manager.ts
@@ -44,54 +44,65 @@ class TransportManager {
    * already a running transport instance, assigns the session to the transport
    * with the least number of assigned sessions.
    *
+   * If there is no config for the given protocol type or any other error
+   * occurs during instantiation of the transport, a Socket.IO transport will
+   * be returned as fallback.
+   *
    * @param protocol Transport procotol to use
    * @param session Session to assign transport to
-   * @returns An instantiated transport, subclass of `ExternalTransport`, depending on `protocol`
+   * @returns An instantiated transport, subclass of `ExternalTransport`, depending on `protocol` or DummyTransport on error
    */
-  private assignExternalTransport(protocol: ExternalTransportType, session: Session): ExternalTransport {
-    // Load config for protocol type
-    const transportConfig: TransportConfig = loadConfigSync(`config/${protocol}-config.json`);
-    const { portMapping } = transportConfig;
+  private assignExternalTransport(protocol: ExternalTransportType, session: Session): ExternalTransport | DummyTransport {
+    try {
+      // Load config for protocol type
+      const transportConfig: TransportConfig = loadConfigSync(`config/${protocol}-config.json`);
+      const { portMapping } = transportConfig;
 
-    // Get all ports delcared in the port mapping
-    const declaredPorts = portMapping.map((p) => p.port );
-    // Get ports of running transports
-    const runningPorts = this.#externalTransports.get(protocol)?.map((t) => t.getPort()) || [];
+      // Get all ports delcared in the port mapping
+      const declaredPorts = portMapping.map((p) => p.port );
+      // Get ports of running transports
+      const runningPorts = this.#externalTransports.get(protocol)?.map((t) => t.getPort()) || [];
 
-    // Get first port which has been declared but not instantiated yet
-    const availablePort = declaredPorts.find((p) => !runningPorts.includes(p));
+      // Get first port which has been declared but not instantiated yet
+      const availablePort = declaredPorts.find((p) => !runningPorts.includes(p));
 
-    // If we found a declared but not instantiated port
-    if (availablePort) {
-      logger.debug("Found unassigned port", availablePort, "for starting", protocol, "transport for session", session.name);
+      // If we found a declared but not instantiated port
+      if (availablePort) {
+        logger.debug("Found unassigned port", availablePort, "for starting", protocol, "transport for session", session.name);
 
-      // Instantiate new transport and return it
-      const transport = ExternalTransportBuilder.instantiate(protocol, EXTERNAL_HOSTNAME, availablePort, transportConfig, session);
-      if (this.#externalTransports.has(protocol)) {
-        this.#externalTransports.get(protocol)?.push(transport);
-      } else {
-        this.#externalTransports.set(protocol, [transport]);
+        // Instantiate new transport and return it
+        const transport = ExternalTransportBuilder.instantiate(protocol, EXTERNAL_HOSTNAME, availablePort, transportConfig, session);
+        if (this.#externalTransports.has(protocol)) {
+          this.#externalTransports.get(protocol)?.push(transport);
+        } else {
+          this.#externalTransports.set(protocol, [transport]);
+        }
+
+        return transport;
       }
 
-      return transport;
+      // Otherwise, return existing transport with least sessions
+      const leastOccupiedTransport = this.#externalTransports.get(protocol)?.sort((a, b) => {
+        if (a.countSessions() < b.countSessions()) {
+          return -1;
+        } else if (b.countSessions() > b.countSessions()) {
+          return 1;
+        }
+
+        return 0;
+      })[0]!;
+
+      logger.debug("Assigning session", session.name, "to", protocol, "transport with", leastOccupiedTransport?.countSessions(), "sessions");
+
+      // Add session to transport
+      leastOccupiedTransport.addSession(session);
+      return leastOccupiedTransport;
+    } catch {
+      logger.warn("No config for protocol type", protocol, "found! Using Socket.IO transport as fallback");
+
+      // Return DummyTransport in case of error
+      return new DummyTransport();
     }
-
-    // Otherwise, return existing transport with least sessions
-    const leastOccupiedTransport = this.#externalTransports.get(protocol)?.sort((a, b) => {
-      if (a.countSessions() < b.countSessions()) {
-        return -1;
-      } else if (b.countSessions() > b.countSessions()) {
-        return 1;
-      }
-
-      return 0;
-    })[0]!;
-
-    logger.debug("Assigning session", session.name, "to", protocol, "transport with", leastOccupiedTransport?.countSessions(), "sessions");
-
-    // Add session to transport
-    leastOccupiedTransport.addSession(session);
-    return leastOccupiedTransport;
   }
 }
 
