646eb3d0ad1203d5088f2c8a107939021f262c85
Thomas Röggla <t.roggla@cwi.nl>
2024-10-18 13:49:25 +0200

    added error handling to logreader script

--- scripts/logreader.js
+++ scripts/logreader.js
@@ -18,4 +18,14 @@ socket.on("connect", () => {
   socket.on("message", ({ timestamp, level, message }) => {
     console.log(timestamp, `${level}:`, message);
   });
+
+  socket.on("error", (err) => {
+    console.log("Socket error:", err.message);
+    process.exit(1);
+  });
+});
+
+socket.on("connect_error", (err) => {
+  console.log("Connection error:", err.message);
+  process.exit(1);
 });
