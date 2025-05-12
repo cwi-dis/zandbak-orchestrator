941ccc4cf8a209aa14c641fd4f0e9cbded9613b8
Thomas Röggla <t.roggla@cwi.nl>
2025-05-12 15:40:51 +0200

    added ChatMessage class to store a session's chat messages

---
+++ app/chat_message.ts
@@ -0,0 +1,46 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { Dict } from "../util";
+import Serializable from "./serializable";
+import User from "./user";
+
+class ChatMessage implements Serializable {
+  #id: string;
+  #sender: User;
+  #message: Dict;
+  #timestamp: Date;
+
+  constructor(sender: User, message: Dict) {
+    this.#id = uuidv4();
+    this.#sender = sender;
+    this.#message = message;
+    this.#timestamp = new Date();
+  }
+
+  public get id(): string {
+    return this.#id;
+  }
+
+  public get sender(): User {
+    return this.#sender;
+  }
+
+  public get message(): Dict {
+    return this.#message;
+  }
+
+  public get timestamp(): Date {
+    return this.#timestamp;
+  }
+
+  public serialize(): Dict {
+    return {
+      id: this.#id,
+      sender: this.#sender.serialize(),
+      message: this.#message,
+      timestamp: this.#timestamp
+    };
+  }
+}
+
+export default ChatMessage;
