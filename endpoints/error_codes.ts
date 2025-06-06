f4bb913802843d2a5c2a07fe104f9f2743021f40
Thomas Röggla <t.roggla@cwi.nl>
2025-06-06 17:08:09 +0200

    added error code for unauthorized actions

--- endpoints/error_codes.ts
+++ endpoints/error_codes.ts
@@ -12,6 +12,7 @@ enum ErrorCodes {
   SESSION_USER_ALREADY_IN_SESSION = 603,
   SESSION_USER_NOT_IN_SESSION = 700,
   SESSION_USER_NOT_IN_ANY_SESSION = 800,
+  SESSION_USER_ACTION_NOT_ALLOWED = 801,
   SESSION_USER_NOT_IN_SAME_SESSION = 802,
 
   USER_DATA_USER_NOT_FOUND = 1301,
@@ -43,6 +44,7 @@ export const ErrorMessages: { [key in ErrorCodes]: string } = {
   [ErrorCodes.SESSION_USER_NOT_IN_SESSION]: "The user has not joined any session",
   [ErrorCodes.SESSION_USER_NOT_IN_ANY_SESSION]: "The user is not in any session",
   [ErrorCodes.SESSION_USER_NOT_IN_SAME_SESSION]: "The target user is not in the same session",
+  [ErrorCodes.SESSION_USER_ACTION_NOT_ALLOWED]: "The user is not allowed to perform this action",
 
   [ErrorCodes.USER_DATA_USER_NOT_FOUND]: "The user was not found",
   [ErrorCodes.USER_DATA_MISSING_DATA_JSON]: "User data (JSON) missing",
