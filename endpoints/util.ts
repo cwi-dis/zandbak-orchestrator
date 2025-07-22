de52c06574757d2e5d91be165603d6a31e7bc3f4
Thomas Röggla <t.roggla@cwi.nl>
2025-07-22 10:31:49 +0300

    changed log level of debug messages in DUMP_DATA endpoint handler

--- endpoints/util.ts
+++ endpoints/util.ts
@@ -41,7 +41,7 @@ const installHandlers = (orchestrator: Orchestrator, socket: Socket) => {
    * Dumps the entire data tree of the orchestrator and sends it to the caller.
    */
   socket.on(EndpointNames.DUMP_DATA, (data, callback) => {
-    logger.debug(EndpointNames.DUMP_DATA, "Dumping all orchestrator data");
+    logger.silly(EndpointNames.DUMP_DATA, "Dumping all orchestrator data");
 
     callback(
       util.createCommandResponse(data, ErrorCodes.OK, orchestrator.serialize())
