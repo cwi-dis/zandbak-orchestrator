936b1ed70b0448e291def31595345fba452e3f5d
Thomas Röggla <t.roggla@cwi.nl>
2025-09-15 16:21:06 +0200

    added constructor and basic getters to Bubble class

--- app/bubble.ts
+++ app/bubble.ts
@@ -3,10 +3,34 @@ import User from "./user";
 import { Dict } from "../util";
 
 class Bubble extends Serializable {
+  #name: string;
+  #owner: User;
   #users: Array<User> = [];
+
+  public constructor(name: string, owner: User) {
+    super();
+
+    this.#name = name;
+    this.#owner = owner;
+    this.#users.push(owner);
+  }
+
+  public get name(): string {
+    return this.#name;
+  }
+
+  public get owner(): User {
+    return this.#owner;
+  }
+
+  public get users(): Array<User> {
+    return this.#users;
+  }
 
   public serialize(): Dict {
     return {
+      name: this.#name,
+      owner: this.#owner,
       users: this.#users.map((u) => u.serialize())
     };
   }
