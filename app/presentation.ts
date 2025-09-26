2d8424e8811b48481d9648f280441fe5f09ce65a
Thomas Röggla <t.roggla@cwi.nl>
2025-09-26 13:49:04 +0200

    added id to constructor for Presentation and added it to serialize()

--- app/presentation.ts
+++ app/presentation.ts
@@ -1,6 +1,7 @@
 import Serializable from "./serializable";
 
 class Presentation extends Serializable {
+  #id: string;
   #name: string;
   #description: string;
   #presenter: string;
@@ -11,14 +12,19 @@ class Presentation extends Serializable {
 
   isSharing = false;
 
-  constructor(name: string, description: string, presenter: string, slidesUrl: string, numSlides: number) {
+  constructor(id: string, name: string, description: string, presenter: string, slidesUrl: string, numSlides: number) {
     super();
 
+    this.#id = id;
     this.#name = name;
     this.#description = description;
     this.#presenter = presenter;
     this.#slidesUrl = slidesUrl;
     this.#numSlides = numSlides;
+  }
+
+  public get id() {
+    return this.#id;
   }
 
   public get name() {
@@ -51,6 +57,7 @@ class Presentation extends Serializable {
 
   public serialize() {
     return {
+      id: this.#id,
       name: this.#name,
       description: this.#description,
       presenter: this.#presenter,
