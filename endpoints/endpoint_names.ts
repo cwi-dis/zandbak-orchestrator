a66b0b5c6294bf5ef1be61d85aafabf8ea0d5fb0
Thomas Röggla <t.roggla@cwi.nl>
2025-10-09 16:42:11 +0200

    added endpoint name for bubble join requests

--- endpoints/endpoint_names.ts
+++ endpoints/endpoint_names.ts
@@ -18,6 +18,7 @@ enum EndpointNames {
   CREATE_BUBBLE = "CreateBubble",
   LEAVE_BUBBLE = "LeaveBubble",
   LIST_BUBBLES = "ListBubbles",
+  REQUEST_JOIN_BUBBLE = "RequestJoinBubble",
 
   GET_USER_DATA = "GetUserData",
   UPDATE_USER_DATA = "UpdateUserDataJson",
