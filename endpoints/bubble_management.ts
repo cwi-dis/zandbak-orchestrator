2f3835a570b49330a150b524d382e7dab1653136
Thomas Röggla <t.roggla@cwi.nl>
2025-11-15 18:22:55 +0100

    removing bubble in LEAVE_BUBBLE if it has become empty

--- endpoints/bubble_management.ts
+++ endpoints/bubble_management.ts
@@ -80,6 +80,11 @@ const installHandlers = (user: User) => {
       bubbleId: bubble.id
     });
 
+    // Removing bubble altogether if bubble is empty
+    if (bubble.users.length == 0) {
+      session.removeBubble(bubble);
+    }
+
     callback(util.createCommandResponse(
       data,
       ErrorCodes.OK,
