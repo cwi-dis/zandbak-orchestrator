bec5083c0d4ea6e726cdde8588ebb05f3e086596
Thomas Röggla <t.roggla@cwi.nl>
2026-03-25 16:39:14 +0100

    added Room class for representing room information

---
+++ app/room.ts
@@ -0,0 +1,36 @@
+import { Dict } from "../util";
+import Serializable from "./serializable";
+
+class Room extends Serializable {
+  #id: string;
+  #name: string;
+  #description: string;
+  #filename: string;
+
+  public get id() {
+    return this.#id;
+  }
+
+  public get name() {
+    return this.#name;
+  }
+
+  public get description() {
+    return this.#description;
+  }
+
+  public get filename() {
+    return this.#filename;
+  }
+
+  public serialize(): Dict {
+    return {
+      id: this.#id,
+      name: this.#name,
+      description: this.#description,
+      filename: this.#filename,
+    };
+  }
+}
+
+export default Room;
